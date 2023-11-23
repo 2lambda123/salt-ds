eaec5082f6ccdf61292db71613e5b37197afd4a8
R662301 <lily.cooksley@jpmchase.com>
2023-11-23 16:26:50 +0000

    prettier

--- site/src/components/keyboard-controls/KeyboardControl.tsx
+++ site/src/components/keyboard-controls/KeyboardControl.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode, Fragment } from "react";
+
 import styles from "./KeyboardControls.module.css";
 
 export interface KeyboardControlProps {
@@ -35,7 +36,11 @@ const Keys: FC<{ keyOrCombos: string }> = ({ keyOrCombos }) => {
         if (index % 2 === 1) {
           return <Fragment key={index}> {keyOrSeparator} </Fragment>;
         }
-        return <kbd key={index} className={styles.key}>{keyOrSeparator}</kbd>;
+        return (
+          <kbd key={index} className={styles.key}>
+            {keyOrSeparator}
+          </kbd>
+        );
       })}
     </>
   );
